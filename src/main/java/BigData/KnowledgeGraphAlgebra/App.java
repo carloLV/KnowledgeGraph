20c285868f6731597400e3389b074a953e015dc4
carloLV <carlo.laviola@gmail.com>
2017-06-21 14:36:27 +0200

    first commit

---
+++ src/main/java/BigData/KnowledgeGraphAlgebra/App.java
@@ -0,0 +1,13 @@
+package BigData.KnowledgeGraphAlgebra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
